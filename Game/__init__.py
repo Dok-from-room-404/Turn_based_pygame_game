aba74ddf915f7b5f5937815c29b49019771fc3e4
Room 404 <dbuhanov366@gmail.com>
2022-12-23 15:34:37 +0900

    Update __init__.py

--- Game/__init__.py
+++ Game/__init__.py
@@ -15,5 +15,18 @@ class Game:
         '''Инициализирует класс.\n
         image – словарь с текстурами для игры'''
         self.image = image
-        
+        # Словарь где каждая текстура привязана к символу (для уровня)
+        self.dic_image_from_level = {
+            ".": self.image["floor_grass"], 
+            ",": self.image["floor_stone"], 
+            "/": self.image["floor_tree"], 
+            
+            "#": self.image["obstacles_stone"], 
+            "$": self.image["obstacles_tree"], 
+            
+            ":": self.image["wall_stone"], 
+            ";": self.image["wall_tree"], 
+            
+            
+        }# "@":  self.dic_image["actor"],
     
