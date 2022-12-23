5a7e82e1aa6983a147602caa26d8801164534b48
Room 404 <dbuhanov366@gmail.com>
2022-12-23 15:00:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,16 @@
 
 
 
+# Импорт модулей
+import pygame
+from pygame.locals import *
+import sys
+
+
+
+
+
+
 # Данный класс необходим для совмещения лаунчера и игры 
 # Основной класс 
 class Main:
@@ -13,13 +23,65 @@ class Main:
         size = width, height = 1_600, 900
         # Маштабирование блоков(Размер блока)
         block_scaling = 100
+        # Выбираем блоки по масштабу 
+        self.load_image(block_scaling)
         
         
     def load_image(self, block_scaling:int=100) -> None:
-        ...
+        '''Необходима для выбора текстур по параметрам из лаунчера '''
+        if block_scaling == 50:
+            self.image = {
+                "floor_grass": pygame.image.load("images\\floor\\grass\\25.png"),
+                "floor_stone": pygame.image.load("images\\floor\\stone\\25.png"),
+                "floor_tree": pygame.image.load("images\\floor\\tree\\25.png"),
+
+                "obstacles_stone": pygame.image.load("images\\obstacles\\stone\\25.png"),
+                "obstacles_tree": pygame.image.load("images\\obstacles\\tree\\25.png"),
+                
+                "wall_stone": pygame.image.load("images\\wall\\stone\\25.png"),
+                "wall_tree": pygame.image.load("images\\wall\\tree\\25.png")
+            }
+
+        elif block_scaling == 100:
+            self.image = {
+                "floor_grass": pygame.image.load("images\\floor\\grass\\50.png"),
+                "floor_stone": pygame.image.load("images\\floor\\stone\\50.png"),
+                "floor_tree": pygame.image.load("images\\floor\\tree\\50.png"),
+
+                "obstacles_stone": pygame.image.load("images\\obstacles\\stone\\50.png"),
+                "obstacles_tree": pygame.image.load("images\\obstacles\\tree\\50.png"),
+
+                "wall_stone": pygame.image.load("images\\wall\\stone\\50.png"),
+                "wall_tree": pygame.image.load("images\\wall\\tree\\50.png")
+            }
+            
+        elif block_scaling == 150:
+            self.image = {
+                "floor_grass": pygame.image.load("images\\floor\\grass\\75.png"),
+                "floor_stone": pygame.image.load("images\\floor\\stone\\75.png"),
+                "floor_tree": pygame.image.load("images\\floor\\tree\\75.png"),
+
+                "obstacles_stone": pygame.image.load("images\\obstacles\\stone\\75.png"),
+                "obstacles_tree": pygame.image.load("images\\obstacles\\tree\\75.png"),
+
+                "wall_stone": pygame.image.load("images\\wall\\stone\\75.png"),
+                "wall_tree": pygame.image.load("images\\wall\\tree\\75.png")
+            }
+            
+        elif block_scaling == 200:
+            self.image = {
+                "floor_grass": pygame.image.load("images\\floor\\grass\\100.png"),
+                "floor_stone": pygame.image.load("images\\floor\\stone\\100.png"),
+                "floor_tree": pygame.image.load("images\\floor\\tree\\100.png"),
+
+                "obstacles_stone": pygame.image.load("images\\obstacles\\stone\\100.png"),
+                "obstacles_tree": pygame.image.load("images\\obstacles\\tree\\100.png"),
+
+                "wall_stone": pygame.image.load("images\\wall\\stone\\100.png"),
+                "wall_tree": pygame.image.load("images\\wall\\tree\\100.png")
+            }
+            
     
-    
-    ...
     
     
 
@@ -27,7 +89,8 @@ class Main:
 
 # Основная функция для работы с программой
 def main():
-    ...
+    program_main = Main()
+    program_main.launcher()
     
     
 if __name__ == "__main__":
