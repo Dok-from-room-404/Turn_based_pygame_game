5dee2fdc606128cfbf5287960594cfeeecd1ef31
Room 404 <dbuhanov366@gmail.com>
2022-12-23 15:20:37 +0900

    Update __init__.py

--- Game/__init__.py
+++ Game/__init__.py
@@ -2,3 +2,18 @@
 
 
 
+import pygame
+from pygame.locals import *
+import sys
+
+
+
+
+# Основной класс для взаимодействия с игрой
+class Game:
+    def __init__(self, image:dict) -> None:
+        '''Инициализирует класс.\n
+        image – словарь с текстурами для игры'''
+        self.image = image
+        
+    
