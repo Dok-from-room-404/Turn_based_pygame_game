24cc94a2bba31f7155158ed78901b034c14b266d
Room 404 <dbuhanov366@gmail.com>
2022-12-25 23:48:48 +0900

    Update __init__.py

--- launcher/__init__.py
+++ launcher/__init__.py
@@ -8,13 +8,48 @@ if __name__ == "__main__":
 else:
     from .GUI import *
 import pickle
+import sys
+import PyQt5
+from PyQt5 import QtWidgets
 
 
 
 
+# Класс лаунчера
 class Launcher:
-    def read_options(self):
+    def show(self) -> None:
+        '''Данный метод показывает окно лаунчера'''
+        options = self.__read_options()
+        
+        # QtWidgets.QApplication - Создает приложение 
+        # QtWidgets.QWidget - Создает окно
+        # Создаем приложение с передачей информации о открытых файлов 
+        # 1 идет имя скрипта остальное - открытые файлы с помощью скрипта
+        self.prog = QtWidgets.QApplication(sys.argv)
+        # Создание окна
+        self.window = QtWidgets.QMainWindow()
+
+        self.GUI = Ui_MainWindow()
+        self.GUI.setupUi(self.window)
+
+        self.window.setWindowTitle("Имя окна")
+        # Корректное отображение окна
+        self.window.show()
+        # Корректное отображение окна
+        sys.exit(self.prog.exec())
+        
+
+        
+
+    def __read_options(self) -> dict:
         '''Данный метод получает словарь из файла параметров'''
-        input()
-        open("launcher\\options.conf")
-        input()
+        file = open("launcher\\options.conf", "rb")
+        options = pickle.load(file)
+        file.close
+        return options
+        
+    def __connect(self) -> None:
+        '''Добавляет функции на элементы интерфейса'''
+        pass
+        
+        
