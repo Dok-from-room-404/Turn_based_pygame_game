5920d3bf875d45fdd1d3c3313e3e20ab56f09288
Room 404 <dbuhanov366@gmail.com>
2023-01-13 23:39:19 +0900

    Update __init__.py

--- Game/__init__.py
+++ Game/__init__.py
@@ -6,6 +6,7 @@
 import pygame
 from pygame.locals import *
 from .Board import *
+from .Menu.menu import *
 
 
 
@@ -15,8 +16,8 @@ class Game:
     def __init__(self, image:dict, size, block_size:int=50) -> None:
         '''Инициализирует класс.\n
         image – словарь с текстурами для игры\n
-        block_size - размер блока\n
-        size - size of screen'''
+        block_size - размер блока
+        size - список указывающий размеры окна игры\n'''
         self.size = size
         self.image = image
         self.block_size = block_size
@@ -40,8 +41,35 @@ class Game:
         self.actor_image = "@"
         # Класс отвечающий за прорисовку уровня
         self.board = Board(self.block_size, self.dic_image_from_level, self.sp_ctop_block, self.actor_image)
+        # Класс отвечающий за меню
+        self.menu = Menu(self.image["fon"])
+        # Флаг загруженного уровня True - загружен, False - не загружен
+        self.stete_load_level = False
+        
+        
+        
+    def show_menu(self, screen, clock_fps):
+        """Отображает титульный экран, пока пользователь не нажмет клавишу\n
+        1) Будем искать сохранения\n
+        2) Будем продолжать игру\n
+        3) Будем выходить из игры"""
+        try:
+            res = self.menu.show_menu(screen, clock_fps, self.stete_load_level)
+            
+            if res == "new_games":
+                self.show_test_level()
+            
+            
+        except BreakError:
+            pygame.quit()
+            return "break"
+        
+        
+        
+        
     def show_test_level(self) -> None:
         '''Загрузка уровня, переход на следующий и т.д. но пока просто показывает тестовый уровень'''
+        self.stete_load_level = True
         self.level = Map(self)
         # камера
         self.camera = Camera(self, self.level.width, self.level.height)
